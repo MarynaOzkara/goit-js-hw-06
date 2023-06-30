ab6ac1acf484e12ca9a11b27fb309700c5ccc4a0
MarynaOzkara <ozkaramaryna@gmail.com>
2023-06-30 18:51:37 +0300

    task9

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,18 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+const body = document.querySelector('body');
+const colorChangeBtn = document.querySelector('.change-color');
+const сolorSpan = document.querySelector('.color');
+
+colorChangeBtn.addEventListener('click', onClick);
+
+function onClick() {
+  let hexColor = getRandomHexColor();
+  function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, 0)}`;
+  }
+  body.style.backgroundColor = hexColor;
+  сolorSpan.textContent = body.style.backgroundColor;
 }
+
+
